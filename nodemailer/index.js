b93a97a84a77ce208b0f9b7e1e66eb943e5e8f56
shaoyulan <poterpoter2@gmail.com>
2025-03-11 07:49:42 +0800

    feat: nodemailer

---
+++ nodemailer/index.js
@@ -0,0 +1,53 @@
+"use strict";
+/**
+ * html email測試寄送工具
+ */
+
+
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    /**
+     * Ethereal is a fake SMTP service, mostly aimed at Nodemailer users (but not limited to). 
+     * It's a completely free anti-transactional email service 
+     * where messages "never get delivered"
+     */
+    // host: "smtp.ethereal.email",
+    // port: 587,
+    // secure: false, // true for 465, false for other ports
+    // auth: {
+    //   user: testAccount.user, // generated ethereal user
+    //   pass: testAccount.pass, // generated ethereal password
+    // },
+    service: 'gmail',
+    auth: {
+        user: 'youremail@gmail.com',
+        pass: 'yourpassword',
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "mukishao@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
